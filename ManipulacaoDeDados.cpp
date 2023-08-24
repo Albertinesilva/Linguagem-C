f9b6a3fca9fe6d6532fd58022ebb48da16474b8d
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-08-24 00:40:14 -0300

    Manipulação de Dados

---
+++ ManipulacaoDeDados.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "Informe um número: " << endl;
+    int num1 = 0;
+    cin >> num1;
+
+    cout << "Informe outro número: " << endl;
+    int num2 = 0;
+    cin >> num2;
+
+    cout << "\n"
+         << num1 
+         << " e " 
+         << num2 
+         << endl;
+
+    return 0;
+}
