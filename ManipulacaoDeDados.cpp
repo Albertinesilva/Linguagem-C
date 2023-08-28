93ca3c0f64020f164ccab9a6d7d10457f4ddf64c
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-08-27 22:14:02 -0300

    Atualizações C++

--- ManipulacaoDeDados.cpp
+++ ManipulacaoDeDados.cpp
@@ -2,22 +2,40 @@
 
 using namespace std;
 
+void vetor(int vect[], int tamanho);
+
 int main()
 {
 
-    cout << "Informe um número: " << endl;
-    int num1 = 0;
-    cin >> num1;
+    int vect[] = {1,2,3,4,5,6,7,8,9,10};
+    int tamanho = 10;
 
-    cout << "Informe outro número: " << endl;
-    int num2 = 0;
-    cin >> num2;
+    vetor(vect, tamanho);
 
-    cout << "\n"
-         << num1 
-         << " e " 
-         << num2 
-         << endl;
+    for(int i = 0; i < tamanho; i++){
+        cout << vect[i] << endl;
+    }
+
+    // cout << "Informe um número: " << endl;
+    // int num1 = 0;
+    // cin >> num1;
+
+    // cout << "Informe outro número: " << endl;
+    // int num2 = 0;
+    // cin >> num2;
+
+    // cout << "\n"
+    //      << num1 
+    //      << " e " 
+    //      << num2 
+    //      << endl;
 
     return 0;
+}
+
+void vetor(int vect[], int tamanho){
+    
+    for(int i = 0; i < tamanho; i++){
+        vect[i] = vect[i] * 2;
+    }
 }
